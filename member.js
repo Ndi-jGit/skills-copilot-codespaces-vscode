27a2cce6c3b895b0fc884238541f73b586ff2867
Josiah Ndirangu <ndirangujosiah1@gmail.com>
2025-03-24 18:40:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+    return {
+        name: "skills",
+        description: "Skills",
+        type: 1,
+        options: [
+        {
+            name: "member",
+            description: "Member to get skills from",
+            type: 6,
+            required: false,
+        },
+        ],
+    };
+
+}
